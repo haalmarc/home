bf1ca7723ccd28df9f67a2e363906e1e28172075
Marcus Haaland <marcus.haaland@bekk.no>
2022-10-10 15:22:21 +0200

    set document title

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,15 @@
 import Navbar from "./Navbar";
 import styles from "../styles/Layout.module.css";
 import Footer from "./Footer";
+import Head from "next/head";
 
 export default function Layout({ children }) {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Marcus Haaland</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Navbar />
       <main>{children}</main>
       <Footer />
