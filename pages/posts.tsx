9b7182a1525313f4536e75ac902f89d4ccb7bc50
Marcus Haaland <marcus.haaland@bekk.no>
2022-10-10 15:20:14 +0200

    revert avoiding cache

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -13,10 +13,7 @@ interface Post {
 }
 
 export const getStaticProps = async () => {
-  const timestampToAvoidrss2jsonOneHourCache = new Date()
-    .getMilliseconds()
-    .toString();
-  const url = `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@marcushaaland?t=${timestampToAvoidrss2jsonOneHourCache}`;
+  const url = `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@marcushaaland`;
   const request = await fetch(url);
   const data = await request.json();
   const posts = data.items as Post[];
