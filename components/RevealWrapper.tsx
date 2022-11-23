c7c927a72a7bc717971cd0867c413b81c3d17913
haalmarc <marcus.haaland@bekk.no>
2022-11-23 22:03:02 +0100

    rewrite queryselector for reveal-animation to use useRef instead

--- components/RevealWrapper.tsx
+++ components/RevealWrapper.tsx
@@ -1,14 +1,44 @@
-import { useEffect } from "react";
-import { reveal } from "../utils";
+import { useEffect, useRef } from "react";
 
 interface IProps {
   children: React.ReactNode;
 }
 
 export default function Layout({ children }: IProps) {
+  const ref = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
-    reveal();
-    window.addEventListener("scroll", reveal);
+    revealOrHideElement(ref.current);
+
+    function revealListener() {
+      revealOrHideElement(ref.current);
+    }
+
+    window.addEventListener("scroll", revealListener);
+    return () => window.removeEventListener("scroll", revealListener);
   }, []);
-  return <div className="reveal">{children}</div>;
+
+  return (
+    <div ref={ref} className="reveal">
+      {children}
+    </div>
+  );
+}
+
+function revealOrHideElement(domElement: HTMLDivElement) {
+  if (!domElement) return;
+
+  // Code inspired by https://alvarotrigo.com/blog/css-animations-scroll/
+  const viewHeightPosition = window.innerHeight;
+  const elementTopPosition = domElement.getBoundingClientRect().top;
+  const pixelsOfElementInViewBeforeReveal = 100;
+
+  const topOfElementIsWithinViewThreshold =
+    elementTopPosition < viewHeightPosition - pixelsOfElementInViewBeforeReveal;
+
+  if (topOfElementIsWithinViewThreshold) {
+    domElement.classList.add("active");
+  } else {
+    domElement.classList.remove("active");
+  }
 }
